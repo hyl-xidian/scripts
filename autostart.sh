fae67fe8fc5895184af959a221bde4d4c9867770
hyl-xidian <hyl.xidian@gmail.com>
2020-08-23 14:52:52 +0800

    v3

--- autostart.sh
+++ autostart.sh
@@ -3,8 +3,8 @@
 /bin/bash ~/scripts/dwm-status.sh &
 #/bin/bash ~/scripts/wp-autochange.sh &
 /bin/bash ~/scripts/wp-change.sh &
-picom -o 0.75 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-picom -b
+picom -o 0.8 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+#picom -b
 /bin/bash ~/scripts/tap-to-click.sh &
 /bin/bash ~/scripts/inverse-scroll.sh &
 #/bin/bash ~/scripts/setxmodmap-colemak.sh &
