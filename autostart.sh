aeeef3e35dbd6b714e4b5458a6cf137fd4741e43
lhy <hyl.xidian@gmail.com>
2020-11-06 21:32:43 +0800

    use orignal v2ray instead of qv2ray

--- autostart.sh
+++ autostart.sh
@@ -4,5 +4,6 @@
 feh --recursive --bg-fill ~/Pictures/Wallpapers/archlinux1.png
 picom -b -C -G --config /home/lhy/.config/picom/picom.conf
 xfce4-power-manager &
-qv2ray &
+v2ray -config /home/lhy/.config/v2ray/lhy.xidian.json &
+#qv2ray &
 /bin/bash ~/scripts/autostart_wait.sh &
