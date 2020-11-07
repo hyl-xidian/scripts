b4cb6b418c6efdb33094bac0477b8e9bf2cfe8fe
lhy <hyl.xidian@gmail.com>
2020-11-07 21:33:40 +0800

    change wallpaper

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /bin/bash ~/scripts/caps-swapescape.sh &
-feh --recursive --bg-fill ~/Pictures/Wallpapers/archlinux1.png
+feh --recursive --bg-fill ~/Pictures/Wallpapers/overwatch_genji_2-wallpaper-1920x1080.png
 picom -b -C -G --config /home/lhy/.config/picom/picom.conf
 xfce4-power-manager &
 v2ray -config /home/lhy/.config/v2ray/lhy.xidian.json &
